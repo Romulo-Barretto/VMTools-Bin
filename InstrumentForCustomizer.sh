44670fddf176ea7f6a070fc43b78f123e2615b32
romulo-barretto <rfbarre@gmail.com>
2023-08-02 23:49:42 -0300

    Fixing and fixing

--- InstrumentForCustomizer.sh
+++ InstrumentForCustomizer.sh
@@ -86,23 +86,23 @@ function clean_my_system () {
     echo "#"
     echo "# Cleaning too many dependencies off unnecessary resources"
     echo "#"
-    echo $var_manual_sudopsw | sudo -S apt clean -y
-    echo $var_manual_sudopsw | sudo -S apt autoclean -y
-    echo $var_manual_sudopsw | sudo -S apt autoremove -y
-    echo $var_manual_sudopsw | sudo -S apt update
-    echo $var_manual_sudopsw | sudo -S apt upgrade
+    echo "$var_manual_sudopsw" | sudo -S apt clean -y
+    echo "$var_manual_sudopsw" | sudo -S apt autoclean -y
+    echo "$var_manual_sudopsw" | sudo -S apt autoremove -y
+    echo "$var_manual_sudopsw" | sudo -S apt update
+    echo "$var_manual_sudopsw" | sudo -S apt upgrade
 }
 
-function all_keys_from_all_children_from_all_schemas (
+function all_keys_from_all_children_from_all_schemas () {
   for current_schema in $(gsettings list-schemas); do
     for current_children in $(gsettings list-children $current_schema); do
       for current_key in $(gsettings list-keys $current_children 2>/dev/null); do
         if [ $? -eq 0 ]; then
-          echo $current_schema' ---> '$current_children' key: '$current_key' Descript: '$(gsettings describe $current_children $current_key 2>/dev/null)
+          echo "$current_schema"' ---> '"$current_children"' key: '"$current_key"' Descript: '"$(gsettings describe "$current_children" "$current_key" 2>/dev/null)"
         else
           echo 'ERR'
         fi
       done
     done
   done
-)
+}
